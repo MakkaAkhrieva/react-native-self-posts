558e3a5115e3b47202758c24834a89e5be212ea2
MakkaAkhrieva <makka.akhrieva00@mail.ru>
2023-04-03 16:04:45 +0300

    add screens

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+import AppLoading from "expo-app-loading";
+import { bootstrap } from "./src/bootstrap";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={bootstrap}
+        onError={(err) => console.log(err)}
+        onFinish={() => setIsReady(true)}
+      />
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Open up App.js to start working on your app!</Text>S
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
