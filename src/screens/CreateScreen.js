3b4e3f750f011d3ef7fc68560397f53725c689cf
MakkaAkhrieva <makka.akhrieva00@mail.ru>
2023-04-06 13:19:24 +0300

    implement add post functionality

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,9 +1,39 @@
-import React, { useLayoutEffect } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import React, { useLayoutEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Image,
+  Button,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { useDispatch } from "react-redux";
+import { THEME } from "../theme";
 import { AppHeaderIcon } from "../components/AppHeaderIcon";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { addPost } from "../store/acions/post";
 
 export const CreateScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const [text, setText] = useState("");
+
+  const img =
+    "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80";
+
+  const saveHandler = () => {
+    const post = {
+      date: new Date().toJSON(),
+      text: text,
+      img: img,
+      booked: false,
+    };
+    dispatch(addPost(post));
+    navigation.navigate("Main");
+  };
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerLeft: () => (
@@ -18,17 +48,46 @@ export const CreateScreen = ({ navigation }) => {
     });
   }, [navigation]);
   return (
-    <View style={styles.center}>
-      <Text>CreateScreen</Text>
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
+    <ScrollView>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={styles.wrapper}>
+          <Text style={styles.title}>Create new post</Text>
+          <TextInput
+            style={styles.textarea}
+            placeholder="Enter post text"
+            value={text}
+            onChangeText={setText}
+            multiline
+          />
+          <Image
+            style={{ width: "100%", height: 200, marginBottom: 10 }}
+            source={{
+              uri: img,
+            }}
+          />
+          <Button
+            title="Create post"
+            color={THEME.MAIN_COLOR}
+            onPress={saveHandler}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  wrapper: {
+    padding: 10,
+  },
+  title: {
+    fontSize: 20,
+    textAlign: "center",
+    fontFamily: "open-regular",
+    marginVertical: 10,
+  },
+  textarea: {
+    padding: 10,
+    marginBottom: 10,
   },
 });
